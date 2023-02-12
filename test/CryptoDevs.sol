a874c9d91606f08c57105793e06b534aa766d29b
activate-glacier-instinct <101533082+activate-glacier-instinct@users.noreply.github.com>
2023-02-12 20:15:02 +0000

    FEAT (tests): adds initial CryptoDevs test
    Removes older Counter examples
    
    Update remappings

---
+++ test/CryptoDevs.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/src/Test.sol";
+import "../src/CryptoDevs.sol";
+
+
+contract CryptoDevsTest is Test {
+    CryptoDevs public testContract;
+    address private whitelistContractAddr;
+    string private metaDataURL;
+
+    function setUp() public {
+        whitelistContractAddr = makeAddr('0xab747b92377f3eF8B2A6646af066Ab0947E19c18');
+        metaDataURL = 'testurl';
+        testContract = new CryptoDevs(metaDataURL, whitelistContractAddr);
+    }
+
+    function testContractInit() public {
+        assertEq(testContract.maxTokenIds(), 20);
+    }
+}
